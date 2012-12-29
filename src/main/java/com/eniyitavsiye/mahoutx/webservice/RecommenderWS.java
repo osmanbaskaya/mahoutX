7741f681ff4195e19d94d4d4b6ac4b1bb670a9e7
mokarakaya <mozgkarakaya@gmail.com>
2012-12-29 16:52:21 +0200

    new method estimatePreference

--- src/main/java/com/eniyitavsiye/mahoutx/webservice/RecommenderWS.java
+++ src/main/java/com/eniyitavsiye/mahoutx/webservice/RecommenderWS.java
@@ -17,7 +17,9 @@ import org.apache.mahout.cf.taste.common.NoSuchItemException;
 import org.apache.mahout.cf.taste.common.NoSuchUserException;
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.common.TopK;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.common.FastIDSet;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.model.jdbc.ConnectionPoolDataSource;
 import org.apache.mahout.cf.taste.impl.model.jdbc.ReloadFromJDBCDataModel;
 import org.apache.mahout.cf.taste.impl.recommender.svd.ALSWRFactorizer;
@@ -94,7 +96,7 @@ public class RecommenderWS {
             } else {
                 recommendations = predictor.get(context).recommend(userId, offset + length);
             }
-            
+
             String[] list = new String[Math.min(length, recommendations.size())];
             for (int i = offset; i < Math.min(list.length + offset, recommendations.size()); i++) {
                 RecommendedItem recommendedItem = recommendations.get(i);
@@ -104,6 +106,19 @@ public class RecommenderWS {
         } catch (TasteException ex) {
             log.log(Level.SEVERE, null, ex);
             return new String[0];
+        }
+    }
+
+    @WebMethod(operationName = "getRecommendationList")
+    public double estimatePreference(
+            @WebParam(name = "context") String context,
+            @WebParam(name = "userId") long userId,
+            @WebParam(name = "itemId") long itemId) throws Exception {
+        try {
+           return predictor.get(context).estimatePreference(userId, itemId);
+        } catch (Exception ex) {
+            log.log(Level.SEVERE, null, ex);
+            throw ex;
         }
     }
 
