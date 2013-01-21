5aff18b7a8a1685219e771a48df9859259bd5da1
Ceyhun Can ÜLKER <ceyhun@ceyhun-asus-ubuntu.(none)>
2013-01-21 23:13:46 +0200

    UserItemIDMappingFunction interface is added.

---
+++ src/main/java/com/eniyitavsiye/mahoutx/common/UserItemIDIndexMapFunction.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eniyitavsiye.mahoutx.common;
+
+/**
+ *
+ * @author ceyhun
+ */
+public interface UserItemIDIndexMapFunction {
+
+	public Integer userIndex(long id);
+	public Integer itemIndex(long id);
+	
+}
