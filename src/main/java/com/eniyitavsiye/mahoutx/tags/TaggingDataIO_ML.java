477ab01345bdcfc0bf72c4b1e50a211c74d0d6a4
Ceyhun Can ÜLKER <ceyhun@ceyhun-asus-ubuntu.(none)>
2013-01-25 12:08:13 +0200

    Tagging data reader class and its test code is moved to tags from tags.io package.

---
+++ src/main/java/com/eniyitavsiye/mahoutx/tags/TaggingDataIO_ML.java
@@ -0,0 +1,117 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eniyitavsiye.mahoutx.tags;
+
+import com.eniyitavsiye.mahoutx.common.UserItemIDIndexMapFunction;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ceyhun
+ */
+public class TaggingDataIO_ML {
+
+	private static final Logger log = Logger.getLogger(TaggingDataIO_ML.class.getName());
+
+	private File filename;
+	private static final String COL_DELIMITER = "::";
+	private static final String CSV_DELIMITER = ",";
+
+	private Set<Long> nonRatedUsers;
+	private Set<Long> nonRatedItems;
+
+	public Set<Long> getNonRatedItems() {
+		return nonRatedItems;
+	}
+
+	public Set<Long> getNonRatedUsers() {
+		return nonRatedUsers;
+	}
+
+	public TaggingDataIO_ML(String filename) {
+		this(new File(filename));
+	}
+
+	public TaggingDataIO_ML(File file) {
+		this.filename = file;
+		this.nonRatedUsers = new HashSet<>();
+		this.nonRatedItems = new HashSet<>();
+	}
+
+	private File getCSVFilename() {
+		String csvName = filename.getName();
+		csvName = csvName.substring(0, csvName.lastIndexOf(".dat")) + ".csv";
+		return new File(filename.getParentFile(), csvName);
+	}
+
+	private void stripTimeConvertCSV() {
+		try (PrintWriter writer = new PrintWriter(new FileWriter(getCSVFilename()), true);
+				 BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] pieces = line.split(COL_DELIMITER);
+				String userID = pieces[0];
+				String itemID = pieces[1];
+				String tag = pieces[2];
+				writer.println(userID + "," + itemID + "," + tag);
+			}
+		} catch (FileNotFoundException e) {
+			log.log(Level.SEVERE, "This should not happen: {0}", e);
+		} catch (IOException e) {
+			log.log(Level.SEVERE, "Original tag file not found: {0}", e);
+		}
+
+	}
+
+	public TaggingData readTaggingData(UserItemIDIndexMapFunction indexMap) throws IOException {
+		if (!getCSVFilename().exists()) {
+			stripTimeConvertCSV();
+		}
+		TaggingData.Builder builder = new TaggingData.Builder(indexMap);
+		try (BufferedReader reader = new BufferedReader(new FileReader(getCSVFilename()))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] pieces = line.split(CSV_DELIMITER);
+				long userID = Long.parseLong(pieces[0]);
+				long itemID = Long.parseLong(pieces[1]);
+				String tag = pieces[2];
+
+				boolean shouldAdd = true;
+
+				boolean userExists = indexMap.userIndex(userID) < indexMap.getUserCount();
+				if (!userExists) {
+					nonRatedUsers.add(userID);
+				}
+				shouldAdd &= userExists;
+
+				boolean itemExists = indexMap.itemIndex(userID) < indexMap.getItemCount();
+				if (!itemExists) {
+					nonRatedItems.add(itemID);
+				}
+				shouldAdd &= itemExists;
+
+				if (shouldAdd) {
+					builder.addTagging(userID, itemID, tag);
+				} else {
+					log.log(Level.FINE, 
+									"User {0} or Item {1} does not exist but tagged as {2}.",
+									new Object[] { userID, itemID, tag });
+				}
+			}
+		}
+		return builder.done();
+	}
+	
+}
--- src/test/java/com/eniyitavsiye/mahoutx/tags/TaggingDataIO_MLTest.java
+++ src/test/java/com/eniyitavsiye/mahoutx/tags/TaggingDataIO_MLTest.java
@@ -2,25 +2,24 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.eniyitavsiye.mahoutx.tags.io;
+package com.eniyitavsiye.mahoutx.tags;
 
 import com.eniyitavsiye.mahoutx.common.UserItemIDIndexMapFunction;
 import com.eniyitavsiye.mahoutx.svdextension.tag.tagcofi.TagCoFiFactorizer;
-import com.eniyitavsiye.mahoutx.tags.TaggingData;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.mahout.cf.taste.common.TasteException;
-import org.apache.mahout.cf.taste.example.grouplens.GroupLensDataModel;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.junit.After;
 import org.junit.AfterClass;
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
 /**
  *
  * @author ceyhun
@@ -46,8 +45,9 @@ public class TaggingDataIO_MLTest {
 	@Before
 	public void setUp() {
 		try {
-			dataModel = new GroupLensDataModel(new File(root, "ratings.dat"));
-			indexMap = new TagCoFiFactorizer(dataModel, TagCoFiFactorizer.SimilarityCalculator.COSINE, 1, 1, 1, 1, 1);
+			dataModel = new FileDataModel(new File(root, "ratings.csv"));
+			indexMap = new TagCoFiFactorizer(dataModel, 
+								TagCoFiFactorizer.SimilarityCalculator.COSINE, 1, 1, 1, 1, 1);
 		} catch (IOException | TasteException ex) {
 			Logger.getLogger(TaggingDataIO_MLTest.class.getName()).log(Level.SEVERE, null, ex);
 		}
@@ -67,10 +67,15 @@ public class TaggingDataIO_MLTest {
 		TaggingDataIO_ML instance = new TaggingDataIO_ML(new File(root, "tags.dat"));
 		TaggingData data = instance.readTaggingData(indexMap);
 		
-		assertEquals(data.getItemNumTagged(4973L, "French"), 4);
-		assertEquals(data.getUserNumTagged(1751L, "French"), 8);
-		assertEquals(data.getTagUsedTotal("French"), 31);
-		//fail("The test case is a prototype.");
+		assertEquals("item 4973 is tagged \"French\" 4 times.", 4,
+						data.getItemNumTagged(4973L, "French"));
+		assertEquals("user 1754 is tagged \"French\" 8 times.", 8,
+						data.getUserNumTagged(1751L, "French"));
+		assertEquals("tag counts should be same by both user and item matrices.", 
+						data.getTagUsedTotal("French"), 
+						data.getTagUsedTotalByItems("French"));
+		assertEquals("tag \"French\" is used 31 times.", 31, 
+						data.getTagUsedTotal("French"));
 	}
 
 }
