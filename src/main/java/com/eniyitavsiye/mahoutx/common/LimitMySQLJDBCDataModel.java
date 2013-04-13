8eb6393a8a4803909b05a39bd50b09639672b7ec
ciuncan <ceyhuncanu@gmail.com>
2013-04-13 18:59:00 +0300

    A bug solved in LimitMySQLJDBCDataModel which caused some item ratings to slip off.
    It assumed the users come ordered, but the order is broken since data is first taken by parts, then that part is sorted.

--- src/main/java/com/eniyitavsiye/mahoutx/common/LimitMySQLJDBCDataModel.java
+++ src/main/java/com/eniyitavsiye/mahoutx/common/LimitMySQLJDBCDataModel.java
@@ -22,21 +22,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
-
+import java.util.Map;
 
 public class LimitMySQLJDBCDataModel extends MySQLJDBCDataModel {
+
+	private static final long serialVersionUID = -2895240379301248474L;
+
 	private static final Logger log = LoggerFactory.getLogger(LimitMySQLJDBCDataModel.class);
 	private final DataSource dataSource;
 	private String userIDColumn;
 	private String itemIDColumn;
 	private String preferenceColumn;
 	private String preferenceTable;
-	
+
 	public LimitMySQLJDBCDataModel(DataSource dataSource,
-			String preferenceTable, String userIDColumn, String itemIDColumn,
-			String preferenceColumn, String timestampColumn) {
+					String preferenceTable, String userIDColumn, String itemIDColumn,
+					String preferenceColumn, String timestampColumn) {
 		super(dataSource, preferenceTable, userIDColumn, itemIDColumn,
-				preferenceColumn, timestampColumn);
+						preferenceColumn, timestampColumn);
 		this.dataSource = dataSource;
 		this.userIDColumn = userIDColumn;
 		this.itemIDColumn = itemIDColumn;
@@ -45,65 +48,62 @@ public class LimitMySQLJDBCDataModel extends MySQLJDBCDataModel {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2895240379301248474L;
-	
 	@Override
 	public FastByIDMap<PreferenceArray> exportWithPrefs() throws TasteException {
 		// TODO Auto-generated method stub
 		log.info("Exporting all data");
 
-	    Connection conn = null;
-	    Statement stmt = null;
-	    ResultSet rs = null;
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
 
-	    FastByIDMap<PreferenceArray> result = new FastByIDMap<PreferenceArray>();
+		FastByIDMap<PreferenceArray> result = new FastByIDMap<>();
 
-	    try {
-    	  conn = dataSource.getConnection();
-	      stmt = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-	      stmt.setFetchDirection(ResultSet.FETCH_FORWARD);
-	      stmt.setFetchSize(getFetchSize());
-	      int offset=0,limit=400000,counter=0;
-	      
-	      String query = "SELECT " + userIDColumn + ", " + itemIDColumn + ", " + preferenceColumn + " FROM " + preferenceTable
-	              + " ORDER BY " + userIDColumn + ", " + itemIDColumn;
+		try {
+			conn = dataSource.getConnection();
+			stmt = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			stmt.setFetchDirection(ResultSet.FETCH_FORWARD);
+			stmt.setFetchSize(getFetchSize());
+			int offset = 0, limit = 400000, counter = 0;
 
-	      Long currentUserID = null;
-	      List<Preference> currentPrefs = Lists.newArrayList();
-	      do{
-	    	  counter=0;
-	    	  query = "SELECT " + userIDColumn + ", " + itemIDColumn + ", " + preferenceColumn + " FROM " + preferenceTable
-		              + " WHERE id > "+offset+" AND id <= "+(offset+limit)+" ORDER BY " + userIDColumn + ", " + itemIDColumn;
-		      log.info("Executing SQL query (offset = {}) : {}", offset, query);
-			  rs = stmt.executeQuery(query);
-		      log.info("query executed");
-		      while (rs.next()) {
-		    	counter++;
-		        long nextUserID = getLongColumn(rs, 1);
-		        if (currentUserID != null && !currentUserID.equals(nextUserID) && !currentPrefs.isEmpty()) {
-		          result.put(currentUserID, new GenericUserPreferenceArray(currentPrefs));
-		          currentPrefs.clear();
-		        }
-		        currentPrefs.add(buildPreference(rs));
-		        currentUserID = nextUserID;
-		      }
-		      if (!currentPrefs.isEmpty()) {
-		        result.put(currentUserID, new GenericUserPreferenceArray(currentPrefs));
-		      }
-		      offset += limit;
-	      }while(counter!=0);
+			String query;
 
-	      return result;
+			FastByIDMap<List<Preference>> currentPrefs = new FastByIDMap<>(500000);
+			do {
+				counter = 0;
+				query = "SELECT " + userIDColumn + ", " + itemIDColumn + ", " + preferenceColumn + " FROM " + preferenceTable
+								+ " WHERE id > " + offset + " AND id <= " + (offset + limit) + " ORDER BY " + userIDColumn + ", " + itemIDColumn;
 
-	    } catch (SQLException sqle) {
-	      log.info("Exception while exporting all data", sqle);
-	      throw new TasteException(sqle);
-	    } finally {
-	      IOUtils.quietClose(rs, stmt, conn);
-	    }
+				log.info("Executing SQL query (offset = {}) : {}", offset, query);
+				rs = stmt.executeQuery(query);
+				log.info("query executed");
+				while (rs.next()) {
+					counter++;
+					long nextUserID = getLongColumn(rs, 1);
+					List<Preference> prefs;
+					if (currentPrefs.containsKey(nextUserID)) {
+ 						prefs = currentPrefs.get(nextUserID);
+					} else {
+						prefs = Lists.newArrayList();
+						currentPrefs.put(nextUserID, prefs);
+					}
+					prefs.add(buildPreference(rs));
+				}
+				offset += limit;
+			} while (counter != 0);
+
+			for (Map.Entry<Long, List<Preference>> entry : currentPrefs.entrySet()) {
+				PreferenceArray array = new GenericUserPreferenceArray(entry.getValue());
+				result.put(entry.getKey(), array);
+			}
+
+			return result;
+
+		} catch (SQLException sqle) {
+			log.info("Exception while exporting all data", sqle);
+			throw new TasteException(sqle);
+		} finally {
+			IOUtils.quietClose(rs, stmt, conn);
+		}
 	}
-
 }
