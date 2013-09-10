5ecef3e34c925b44d5e0a397a2985a372ad1a6eb
tevfik <tevfik@tevfik-desktop>
2013-09-10 17:15:29 +0300

    Factorizer'i yazmaya devam ediyorum.

--- src/main/java/com/eniyitavsiye/mahoutx/svd/TevfikSVDFactorizer.java
+++ src/main/java/com/eniyitavsiye/mahoutx/svd/TevfikSVDFactorizer.java
@@ -4,11 +4,14 @@
  */
 package com.eniyitavsiye.mahoutx.svd;
 
+import com.google.common.collect.Lists;
+import java.util.List;
 import java.util.Random;
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.common.FullRunningAverage;
 import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
 import org.apache.mahout.cf.taste.impl.common.RunningAverage;
+import org.apache.mahout.cf.taste.impl.model.GenericPreference;
 import org.apache.mahout.cf.taste.impl.recommender.svd.AbstractFactorizer;
 import org.apache.mahout.cf.taste.impl.recommender.svd.Factorization;
 import org.apache.mahout.cf.taste.model.DataModel;
@@ -43,6 +46,7 @@ public class TevfikSVDFactorizer extends AbstractFactorizer {
     private double[][] itemVectors;
     private double defaultValue;
     private double interval;
+    private List<GenericPreference> preferences;
 
     public TevfikSVDFactorizer(DataModel dataModel, int numFeatures, int numIterations) throws TasteException {
         this(dataModel, numFeatures, DEFAULT_LEARNING_RATE, DEFAULT_REGULARIZATION, DEFAULT_RANDOM_NOISE,
@@ -55,7 +59,6 @@ public class TevfikSVDFactorizer extends AbstractFactorizer {
         this.dataModel = dataModel;
         this.numFeatures = numFeatures;
         this.numIterations = numIterations;
-
         this.learningRate = learningRate;
         this.regularization = regularization;
         this.randomNoise = randomNoise;
@@ -82,7 +85,46 @@ public class TevfikSVDFactorizer extends AbstractFactorizer {
                 itemVectors[itemIndex][feature] = defaultValue + (random.nextDouble() - 0.5) * interval * randomNoise;
             }
         }
-        
+
+        preferences = Lists.newArrayListWithCapacity(dataModel.getNumUsers());
+        getPreferences();
+/*
+        for (int iter = 0; iter < numIterations; iter++) {
+            int vgError = 0;
+            int avgAbsError = 0;
+            for (int i = 0; i < TRAIN_SIZE; i++) {
+                user = (int) model.dataArray[i][0];
+                item = (int) model.dataArray[i][1];
+                //System.out.println(i+" "+user+" "+item);
+                error = model.dataArray[i][2] - prediction(user, item);
+
+                f = user_dev.get(user);
+                user_dev.put(user, f + gamma * (error - lambda * f));
+                f = item_dev.get(item);
+                item_dev.put(item, f + gamma * (error - lambda * f));
+
+                ufactor = user_factor.get(user);
+                temp = ufactor.clone();
+                ifactor = item_factor.get(item);
+
+                for (j = 0; j < FACTOR_SIZE; j++) {
+                    ufactor[j] = ufactor[j] + gamma * (error * ifactor[j] - lambda * ufactor[j]);
+                    ifactor[j] = ifactor[j] + gamma * (error * temp[j] - lambda * ifactor[j]);
+                }
+                user_factor.put(user, ufactor);
+                item_factor.put(item, ifactor);
+                avgAbsError = avgAbsError + Math.abs(error);
+                avgError = avgError + error;
+                //		if (i % 1000 == 0)
+                //			System.out.println("AvgError: "+avgAbsError/(i+1));
+            }
+            avgError = avgError / TRAIN_SIZE;
+            System.out.println("AvgError (Training set): " + avgAbsError / TRAIN_SIZE);
+        }
+*/
+
+
+
         return createFactorization(userVectors, itemVectors);
     }
 
@@ -95,5 +137,15 @@ public class TevfikSVDFactorizer extends AbstractFactorizer {
             }
         }
         return average.getAverage();
+    }
+
+    private void getPreferences() throws TasteException {
+        preferences.clear();
+        LongPrimitiveIterator userIDs = dataModel.getUserIDs();
+        while (userIDs.hasNext()) {
+            for (Preference pref : dataModel.getPreferencesFromUser(userIDs.nextLong())) {
+                preferences.add(new GenericPreference(pref.getUserID(), pref.getItemID(), pref.getValue()));
+            }
+        }
     }
 }
